1fc0ae99e1158368cef34cd892b954c75076cbb9
alina704 <60064987+alina704@users.noreply.github.com>
2021-06-03 13:40:37 +0300

    код
    код

---
+++ Pages/Registration.xaml.cs
@@ -0,0 +1,94 @@
+﻿using BooksReader.App;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using WindowChrome.Demo.Pages;
+
+namespace WindowChrome.Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void PrevPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            //App.ParentWindowRef.ParentFrame.Navigate(App.WelcomeRef);
+            NavigationService.GoBack();
+        }
+
+
+
+        private void SingupButton_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User
+            {
+                firstname = ((TextBox)FindName("firstnameTextBox")).Text,
+                lastname = ((TextBox)FindName("lastnameTextBox")).Text,
+                patronymic = ((TextBox)FindName("patronymicTextBox")).Text,
+                username = ((TextBox)FindName("usernameTextBox")).Text,
+                password = ((TextBox)FindName("passwordTextBox")).Text
+            };
+
+            if (user.firstname.Equals("") ||
+                user.lastname.Equals("") ||
+                user.patronymic.Equals("") ||
+                user.username.Equals("") ||
+                user.password.Equals(""))
+            {
+                ((Label)FindName("errorLabel")).Content = "Заполните все поля для регистрации.";
+                ((Label)FindName("errorLabel")).Visibility = Visibility.Visible;
+                return;
+            }
+            try
+            {
+                SaveNewUser(user);
+                App.ParentWindowRef.ParentFrame.Navigate(new BooksAccount(user.username, user.password));
+                App.RegistrationRef = null;
+            }
+            catch (NameAlreadyExistsException)
+            {
+                ((Label)FindName("errorLabel")).Content = "Такой же логин уже существует.";
+                ((Label)FindName("errorLabel")).Visibility = Visibility.Visible;
+            }
+            catch (Exception)
+            {
+                ((Label)FindName("errorLabel")).Content = "Произошли технические неполадки. Повторите регистрацию позднее.";
+                ((Label)FindName("errorLabel")).Visibility = Visibility.Visible;
+            }
+        }
+
+        private void SaveNewUser(User user)
+        {
+            using (EntityContext db = new EntityContext())
+            {
+                if (UserExists(db, user.username))
+                    throw new NameAlreadyExistsException("This username already exists.");
+
+                db.Users.AddRange(user);
+                db.SaveChanges();
+            }
+        }
+
+        private bool UserExists(EntityContext entityContext, string username)
+        {
+            int id = 0;
+            id = entityContext.Users.
+                Where(u => u.username == username).
+                Select(u => u.Id).
+                SingleOrDefault();
+            if (id != 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Pages/Welcome.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using WindowChrome.Demo.Pages;
+
+namespace WindowChrome.Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для Welcome.xaml
+    /// </summary>
+    public partial class Welcome : Page
+    {
+        public Welcome()
+        {
+
+            InitializeComponent();
+        }
+
+        private void SingUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.RegistrationRef == null)
+            {
+                App.RegistrationRef = new Registration();
+            }
+            App.ParentWindowRef.ParentFrame.Navigate(App.RegistrationRef);
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = ((TextBox)FindName("usernameTextBox")).Text;
+            string password = ((TextBox)FindName("passwordTextBox")).Text;
+            if (username.Equals("") || password.Equals(""))
+            {
+                ((Label)FindName("errorLoginLabel")).Visibility = Visibility.Visible;
+                return;
+            }
+            try
+            {
+                App.ParentWindowRef.ParentFrame.Navigate(new BooksAccount(username, password));
+            }
+            catch (UserNotFoundException)
+            {
+                ((Label)FindName("errorLoginLabel")).Visibility = Visibility.Visible;
+            }
+            catch (Exception)
+            {
+                ((Label)FindName("errorLoginLabel")).Content = "Произошли технические неполадки. Повторите вход позднее.";
+                ((Label)FindName("errorLoginLabel")).Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
